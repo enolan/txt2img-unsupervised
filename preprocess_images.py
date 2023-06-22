d38f70728399f280cc7141e18850c1d910ce8330
Echo Nolan <echo@echonolan.net>
2023-06-22 22:34:02 +0000

    concurrency amirite

--- preprocess_images.py
+++ preprocess_images.py
@@ -13,6 +13,7 @@ import pandas as pd
 import PIL.Image
 import pyarrow as pa
 import pyarrow.parquet as pq
+import queue
 import random
 import torch
 import transformers
@@ -197,7 +198,7 @@ queued_img_semaphore = Semaphore(args.batch_size * 4)
 # the head image queue but PIL threads can be waiting on the global limit, which isn't released
 # until the main thread dequeues stuff. Hacky, but we can avoid this by having a "backup" semaphore
 # for each directory that's only used when the global limit is reached. They start empty and are
-# incremented to 1 when the main thread starts processing that directory.
+# incremented to 1 if the main thread blocks long enough waiting for that dir.
 per_dir_semaphores = [Semaphore(0) for _ in in_dirs]
 
 # Thread pool for loading/scaling/cropping images
@@ -281,8 +282,7 @@ embedded_batches_j = {0: []}
 embedded_batches_np = {0: []}
 # The paths of the images that have been encoded
 encoded_imgs_paths = {0: []}
-# There is always capacity for at least one image in any live queue
-per_dir_semaphores[0].release()
+
 
 with tqdm(total=len(in_dirs), desc="directories") as dirs_pbar:
     with tqdm(desc="files") as files_pbar:
@@ -294,7 +294,16 @@ with tqdm(total=len(in_dirs), desc="directories") as dirs_pbar:
             batch_dir_indices = []
             while len(batch) < args.batch_size:
                 try:
-                    imgs, img_path = imgs_queues[img_queue_idx].get()
+                    while True:
+                        try:
+                            imgs, img_path = imgs_queues[img_queue_idx].get(timeout=1)
+                            break
+                        except queue.Empty:
+                            # See comment above re potential deadlock
+                            tqdm.write(
+                                f"Images for dir #{img_queue_idx} coming in slowly, poking relief semaphore"
+                            )
+                            per_dir_semaphores[img_queue_idx].release()
                     if imgs is not None:
                         batch.append(imgs)
                         encoded_imgs_paths[img_queue_idx].append(img_path)
@@ -304,11 +313,9 @@ with tqdm(total=len(in_dirs), desc="directories") as dirs_pbar:
                         ):
                             batch_dir_indices.append((img_queue_idx, len(batch) - 1))
                         queued_img_semaphore.release()
-                        per_dir_semaphores[img_queue_idx].release()
                     else:
                         # This image was skipped
                         queued_img_semaphore.release()
-                        per_dir_semaphores[img_queue_idx].release()
                         files_pbar.update(1)
                 except CloseableQueue.Closed:
                     # This queue is closed, move on to the next one
@@ -327,7 +334,6 @@ with tqdm(total=len(in_dirs), desc="directories") as dirs_pbar:
                         embedded_batches_j[img_queue_idx] = []
                         embedded_batches_np[img_queue_idx] = []
                         encoded_imgs_paths[img_queue_idx] = []
-                        per_dir_semaphores[img_queue_idx].release()
                         tqdm.write(
                             f"Moving to image queue {img_queue_idx} ({in_dirs[img_queue_idx]})"
                         )
