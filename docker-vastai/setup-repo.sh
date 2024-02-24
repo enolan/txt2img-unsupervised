8b29bd85665ea04429bc9deec2c5896ca4d767e6
Echo Nolan <echo@echonolan.net>
2024-02-24 02:48:43 +0000

    Remove poet-plugin from docker image + add repo fetch script

---
+++ docker-vastai/setup-repo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Set up dev environment
+git clone --recursive https://github.com/enolan/txt2img-unsupervised.git /root/txt2img-unsupervised
+ln -sf /root/vq-f4/model.ckpt /root/txt2img-unsupervised/vq-f4.ckpt
+cd /root/txt2img-unsupervised
+poetry install --with dev --with cuda --no-root
