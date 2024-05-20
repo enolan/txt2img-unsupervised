5f42cbc672ed17ea3ce8556e88081eaeee8d09f2
Echo Nolan <echo@echonolan.net>
2024-05-20 01:09:17 +0000

    Make repo setup script more reliable

--- docker-vastai/setup-repo.sh
+++ docker-vastai/setup-repo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Set up dev environment
 git clone --recursive https://github.com/enolan/txt2img-unsupervised.git /root/txt2img-unsupervised
 ln -sf /root/vq-f4/model.ckpt /root/txt2img-unsupervised/vq-f4.ckpt
