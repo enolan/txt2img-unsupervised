da6ea88d3bb30a3d81d9960024fec5f016ed7489
Echo Nolan <echo@echonolan.net>
2024-09-17 04:13:13 +0000

    Fix GPU check

--- txt2img_unsupervised/gpu_check.py
+++ txt2img_unsupervised/gpu_check.py
@@ -9,12 +9,12 @@ def gpu_is_ampere_or_newer() -> bool:
         return False
     device_kind = gpu_devices[0].device_kind
     # See https://en.wikipedia.org/wiki/List_of_Nvidia_graphics_processing_units
-    rtx_3k = [f"RTX 30{i:02d}" for i in range(1, 91, 10)]
-    rtx_4k = [f"RTX 40{i:02d}" for i in range(1, 91, 10)]
+    rtx_3k = [f"RTX 30{i:02d}" for i in range(0, 91, 10)]
+    rtx_4k = [f"RTX 40{i:02d}" for i in range(0, 91, 10)]
     datacenter_ampere_gpus = [f"A{i}" for i in [2, 10, 16, 30, 40, 100]]
     datacenter_hopper_and_lovelace_gpus = ["H100", "L40", "L4"]
 
     acceptable_gpus = (
         rtx_3k + rtx_4k + datacenter_ampere_gpus + datacenter_hopper_and_lovelace_gpus
     )
-    return device_kind in acceptable_gpus
+    return any(gpu_substr in device_kind for gpu_substr in acceptable_gpus)
