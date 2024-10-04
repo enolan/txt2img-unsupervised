54c5883d7d1b4890a4339e20c4e2c7f2449ceea6
Echo Nolan <echo@echonolan.net>
2024-10-04 03:04:12 +0000

    touch a file when dependency install is done in containers

--- docker/setup-repo.sh
+++ docker/setup-repo.sh
@@ -7,3 +7,6 @@ git clone --recursive https://github.com/enolan/txt2img-unsupervised.git /root/t
 ln -sf /root/vq-f4/model.ckpt /root/txt2img-unsupervised/vq-f4.ckpt
 cd /root/txt2img-unsupervised
 poetry install --with dev --with cuda --no-root
+
+# mark success
+touch /root/setup-repo.success
