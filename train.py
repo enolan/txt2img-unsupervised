ab1ffebcbff99d584538564d327c42efe3017735
Echo Nolan <echo@echonolan.net>
2025-03-07 22:16:29 -0500

    train.py: tiny refactor

--- train.py
+++ train.py
@@ -944,8 +944,7 @@ def save_checkpoint_and_log_images(
         tqdm.write("Sampling")
         sample_and_log(my_train_state, sample_batch_size, global_step)
         tqdm.write("Done sampling")
-        visualization_imgs = test_imgs[sfw_indices[:8]]
-        visualization_img_names = visualization_imgs["name"]
+        visualization_img_names = visualization_dset["name"]
         (
             visualization_img_encodings,
             visualization_embeddings,
